14efa8eac65c5b339b160d6bf1e123c25c166b4b
SiiriKukk <siiriraudsepp90@gmail.com>
2023-07-10 22:11:27 +0300

    My commets about the commit

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -4,7 +4,7 @@ describe('Issue deleting', () => {
     cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
         cy.visit(url + '/board');
         cy.get('[data-testid="board-list:backlog"]').children().should('have.length',4)
-        cy.get('[data-testid="board-list:backlog"]').eq(0).contains('This is an issue of type: Task.').click();
+        cy.contains('This is an issue of type: Task.').click();
         cy.get('[data-testid="modal:issue-details"]').should('be.visible')
     });
   });
@@ -13,23 +13,20 @@ describe('Issue deleting', () => {
   let IssueTitle = 'This is an issue of type: Task.'
 
     it('Should delete the first issue successfully', () => {
-    getIssueDetailsModal()
-        .get('[data-testid="icon:trash"]')
-        .click();
+    getIssueDetailsModal().get('[data-testid="icon:trash"]').click();
     //Assert that confirmation modal is visible and delete button active
     cy.get('[data-testid="modal:confirm"]').should('be.visible');
     cy.get('[data-testid="modal:confirm"]').contains('button', 'Delete issue').click();
     //Assert that confirmation modal is not visible
     cy.get('[data-testid="modal:confirm"]').should('not.exist');
     //Assert that issue is deleted
+    cy.reload();
     cy.get('[data-testid="board-list:backlog"]').children().should('have.length',3);
     cy.get('[data-testid="board-list:backlog"]').should('not.contain', IssueTitle);
     });
 
     it('Should cancel the deletion process of first issue successfully', () => {
-        getIssueDetailsModal()
-            .get('[data-testid="icon:trash"]')
-            .click();
+        getIssueDetailsModal().get('[data-testid="icon:trash"]').click();
         //Assert that confirmation modal is visible, cancel button active
         cy.get('[data-testid="modal:confirm"]').should('be.visible');
         cy.get('[data-testid="modal:confirm"]').contains('button', 'Cancel').click();
@@ -37,6 +34,7 @@ describe('Issue deleting', () => {
         cy.get('[data-testid="modal:confirm"]').should('not.exist');
         //Assert that issue is not deleted and seen on the board
         cy.get('[data-testid="icon:close"]').first().click();
+        cy.reload();
         cy.get('[data-testid="board-list:backlog"]').children().should('have.length',4);
         cy.get('[data-testid="board-list:backlog"]').contains(IssueTitle);
         });
