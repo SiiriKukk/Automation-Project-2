bc8b43e34fe4d8738382f2974edcc66f235012d8
SiiriKukk <siiriraudsepp90@gmail.com>
2023-08-03 00:42:50 +0100

    My commets about the commit

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,29 +1,29 @@
-import { faker } from "@faker-js/faker";
-describe("Issue create", () => {
+import { faker } from '@faker-js/faker';
+describe('Issue create', () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit('/');
     cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .should('eq', `${Cypress.env('baseUrl')}project/board`)
       .then((url) => {
         //System will already open issue creating modal in beforeEach block
-        cy.visit(url + "/board?modal-issue-create=true");
+        cy.visit(url + '/board?modal-issue-create=true');
       });
   });
 
-  it("Should create an issue and validate it successfully", () => {
+  it('Should create an issue and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       //open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]').trigger("click");
+      cy.get('[data-testid="select-option:Story"]').trigger('click');
 
       //Type value to description input field
-      cy.get(".ql-editor").type("TEST_DESCRIPTION");
+      cy.get('.ql-editor').type('TEST_DESCRIPTION');
 
       //Type value to title input field
       //Order of filling in the fields is first description, then title on purpose
       //Otherwise filling title first sometimes doesn't work due to web page implementation
-      cy.get('input[name="title"]').type("TEST_TITLE");
+      cy.get('input[name="title"]').type('TEST_TITLE');
 
       //Select Lord Gaben from reporter dropdown
       cy.get('[data-testid="select:userIds"]').click();
@@ -34,152 +34,152 @@ describe("Issue create", () => {
     });
 
     //Assert that modal window is closed and successful message is visible
-    cy.get('[data-testid="modal:issue-create"]').should("not.exist");
-    cy.contains("Issue has been successfully created.").should("be.visible");
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
 
     //Reload the page to be able to see recently created issue
     //Assert that successful message has dissappeared after the reload
     cy.reload();
-    cy.contains("Issue has been successfully created.").should("not.exist");
+    cy.contains('Issue has been successfully created.').should('not.exist');
 
     //Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog')
-      .should("be.visible")
-      .and("have.length", "1")
+      .should('be.visible')
+      .and('have.length', '1')
       .within(() => {
         //Assert that this list contains 5 issues and first element with tag p has specified text
         cy.get('[data-testid="list-issue"]')
-          .should("have.length", "5")
+          .should('have.length', '5')
           .first()
-          .find("p")
-          .contains("TEST_TITLE");
+          .find('p')
+          .contains('TEST_TITLE');
         //Assert that correct avatar and type icon are visible
-        cy.get('[data-testid="avatar:Lord Gaben"]').should("be.visible");
-        cy.get('[data-testid="icon:story"]').should("be.visible");
+        cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
+        cy.get('[data-testid="icon:story"]').should('be.visible');
       });
   });
 
-  it("Test 1 - Should create an issue and validate it successfully", () => {
+  it('Test 1 - Should create an issue and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       //Type value to description input field
-      cy.get(".ql-editor").type("My bug description");
+      cy.get('.ql-editor').type('My bug description');
 
       //Type value to title input field
-      cy.get('input[name="title"]').type("Bug");
+      cy.get('input[name="title"]').type('Bug');
 
       //open issue type dropdown and choose Bug
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Bug"]').trigger("click");
+      cy.get('[data-testid="select-option:Bug"]').trigger('click');
 
       //Select priority
       cy.get('[data-testid="select:priority"]').click();
-      cy.get('[data-testid="select-option:Highest"]').trigger("click");
+      cy.get('[data-testid="select-option:Highest"]').trigger('click');
 
       //Select Pickle Rick from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
       cy.get('[data-testid="select-option:Pickle Rick"]').click();
 
       //assert all values are correctly visible
-      cy.get('input[name="title"]').should("have.value", "Bug");
-      cy.get(".ql-editor").should("have.text", "My bug description");
-      cy.get('[data-testid="select:type"]').should("have.text", "Bug");
+      cy.get('input[name="title"]').should('have.value', 'Bug');
+      cy.get('.ql-editor').should('have.text', 'My bug description');
+      cy.get('[data-testid="select:type"]').should('have.text', 'Bug');
       cy.get('[data-testid="select:reporterId"]').should(
-        "have.text",
-        "Pickle Rick"
+        'have.text',
+        'Pickle Rick'
       );
-      cy.get('[data-testid="select:priority"]').should("have.text", "Highest");
+      cy.get('[data-testid="select:priority"]').should('have.text', 'Highest');
 
       //Click on button "Create issue"
       cy.get('button[type="submit"]').click();
     });
 
     //Assert that modal window is Closed and successful message is visible
-    cy.get('[data-testid="modal:issue-create"]').should("not.exist");
-    cy.contains("Issue has been successfully created.").should("be.visible");
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
 
     //Assert that successful message has dissappeared after the reload
     cy.reload();
-    cy.contains("Issue has been successfully created.").should("not.exist");
+    cy.contains('Issue has been successfully created.').should('not.exist');
 
     //Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog')
-      .should("be.visible")
-      .and("have.length", "1")
+      .should('be.visible')
+      .and('have.length', '1')
       .within(() => {
         //Assert that this list contains 5 issues and first element with tag p has specified text
         cy.get('[data-testid="list-issue"]')
-          .should("have.length", "5")
+          .should('have.length', '5')
           .first()
-          .find("p")
-          .contains("Bug");
+          .find('p')
+          .contains('Bug');
         //Assert that correct type icon is visible
-        cy.get('[data-testid="icon:bug"]').should("be.visible");
+        cy.get('[data-testid="icon:bug"]').should('be.visible');
       });
   });
 
   const randomDescription = faker.lorem.words(5);
   const randomTitle = faker.lorem.word();
-  it("Test 2 - Should create an issue and validate it successfully", () => {
+  it('Test 2 - Should create an issue and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       //Type value to description input field
-      cy.get(".ql-editor").type(randomDescription);
+      cy.get('.ql-editor').type(randomDescription);
 
       //Type value to title input field
       cy.get('input[name="title"]').type(randomTitle);
 
       //open issue type dropdown and check that Task icon is visible and selected
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="icon:task"]').should("be.visible");
+      cy.get('[data-testid="icon:task"]').should('be.visible');
 
       //Select priority
       cy.get('[data-testid="select:priority"]').click();
-      cy.get('[data-testid="select-option:Low"]').trigger("click");
+      cy.get('[data-testid="select-option:Low"]').trigger('click');
 
       //Select Baby Yoda from reporter dropdown
       cy.get('[data-testid="select:reporterId"]').click();
       cy.get('[data-testid="select-option:Baby Yoda"]').click();
 
       //assert all values are correctly visible
-      cy.get('input[name="title"]').should("have.value", randomTitle);
-      cy.get(".ql-editor").should("have.text", randomDescription);
-      cy.get('[data-testid="select:type"]').should("have.text", "Task");
+      cy.get('input[name="title"]').should('have.value', randomTitle);
+      cy.get('.ql-editor').should('have.text', randomDescription);
+      cy.get('[data-testid="select:type"]').should('have.text', 'Task');
       cy.get('[data-testid="select:reporterId"]').should(
-        "have.text",
-        "Baby Yoda"
+        'have.text',
+        'Baby Yoda'
       );
-      cy.get('[data-testid="select:priority"]').should("have.text", "Low");
+      cy.get('[data-testid="select:priority"]').should('have.text', 'Low');
 
       //Click on button "Create issue"
       cy.get('button[type="submit"]').click();
     });
 
     //Assert that modal window is closed and successful message is visible
-    cy.get('[data-testid="modal:issue-create"]').should("not.exist");
-    cy.contains("Issue has been successfully created.").should("be.visible");
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
 
     //Assert that successful message has dissappeared after the reload
     cy.reload();
-    cy.contains("Issue has been successfully created.").should("not.exist");
+    cy.contains('Issue has been successfully created.').should('not.exist');
 
     //Assert than only one list with name Backlog is visible and do steps inside of it
     cy.get('[data-testid="board-list:backlog')
-      .should("be.visible")
-      .and("have.length", "1")
+      .should('be.visible')
+      .and('have.length', '1')
       .within(() => {
         //Assert that this list contains 5 issues and first element with tag p has specified text
         cy.get('[data-testid="list-issue"]')
-          .should("have.length", "5")
+          .should('have.length', '5')
           .first()
-          .find("p")
+          .find('p')
           .contains(randomTitle);
         //Assert that correct type icon is visible
-        cy.get('[data-testid="icon:task"]').first().should("be.visible");
+        cy.get('[data-testid="icon:task"]').first().should('be.visible');
       });
   });
 
-  it("Should validate title is required field if missing", () => {
+  it('Should validate title is required field if missing', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       //Try to click create issue button without filling any data
@@ -187,14 +187,14 @@ describe("Issue create", () => {
 
       //Assert that correct error message is visible
       cy.get('[data-testid="form-field:title"]').should(
-        "contain",
-        "This field is required"
+        'contain',
+        'This field is required'
       );
     });
   });
 
-  it("Should remove unnecessary spaces on the board view", () => {
-    const issueTitle = "  Hello world   ";
+  it('Should remove unnecessary spaces on the board view', () => {
+    const issueTitle = '  Hello world   ';
     const getIssueCreateModal = () =>
       cy.get('[data-testid="modal:issue-create"]');
     const getIssueDetailsModal = () =>
@@ -203,17 +203,17 @@ describe("Issue create", () => {
 
     //Create a new issue
     getIssueCreateModal().within(() => {
-      cy.get(".ql-editor").type("My world spins around");
+      cy.get('.ql-editor').type('My world spins around');
       cy.get('input[name="title"]').type(issueTitle);
       cy.get('button[type="submit"]').click();
     });
-    getIssueCreateModal().should("not.exist");
+    getIssueCreateModal().should('not.exist');
     cy.reload();
     //Access new issue and assert the title
     cy.get('[data-testid="list-issue"]').first().click();
     getIssueDetailsModal().within(() => {
       actualTitle()
-        .invoke("text")
+        .invoke('text')
         .then((actualTitle) => {
           actualTitle.trim();
           expect(actualTitle.trim()).to.equal(issueTitle.trim());
--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,25 +1,25 @@
-describe("Issue time-tracking functionality", () => {
+describe('Issue time-tracking functionality', () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit('/');
     cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .should('eq', `${Cypress.env('baseUrl')}project/board`)
       .then((url) => {
-        cy.visit(url + "/board");
+        cy.visit(url + '/board');
         cy.get('[data-testid="icon:plus"]').click();
         getIssueCreateModal().within(() => {
           cy.get('[data-testid="select:type"]').click();
           cy.get('[data-testid="select-option:Story"]').click();
-          cy.get(".ql-editor").type(issueDescription);
+          cy.get('.ql-editor').type(issueDescription);
           cy.get('input[name="title"]').type(issueTitle);
           cy.get('[data-testid="select:userIds"]').click();
           cy.get('[data-testid="select-option:Lord Gaben"]').click();
           cy.get('button[type="submit"]').click();
         });
         //Assert that new issue is created and opened
-        cy.contains("Issue has been successfully created.").should(
-          "be.visible"
+        cy.contains('Issue has been successfully created.').should(
+          'be.visible'
         );
-        cy.get(backlogList).should("be.visible").contains(issueTitle).click();
+        cy.get(backlogList).should('be.visible').contains(issueTitle).click();
       });
   });
 
@@ -29,83 +29,83 @@ describe("Issue time-tracking functionality", () => {
     cy.get('[data-testid="modal:issue-create"]');
   const getTrackingModal = () => cy.get('[data-testid="modal:tracking"]');
   const backlogList = '[data-testid="board-list:backlog"]';
-  const issueTitle = "Time-tracking";
-  const issueDescription = "Testing time tracking functionality";
+  const issueTitle = 'Time-tracking';
+  const issueDescription = 'Testing time tracking functionality';
   const closeDetailModalButton = '[data-testid="icon:close"]';
   const timeTracking = '[data-testid="icon:stopwatch"]';
 
-  it("Should add, update and remove estimated time", () => {
+  it('Should add, update and remove estimated time', () => {
     //Add estimated time
     getIssueDetailsModal().within(() => {
-      cy.contains("No time logged").should("be.visible");
+      cy.contains('No time logged').should('be.visible');
       cy.get('[placeholder="Number"]').type(10);
-      cy.get('[placeholder="Number"]').should("have.value", 10);
-      cy.contains("10h estimated").should("be.visible");
+      cy.get('[placeholder="Number"]').should('have.value', 10);
+      cy.contains('10h estimated').should('be.visible');
       cy.get(closeDetailModalButton).first().click();
     });
     //Assert that estimated time is saved
-    cy.get(backlogList).should("be.visible").contains(issueTitle).click();
-    cy.get('[placeholder="Number"]').should("have.value", 10);
-    cy.contains("10h estimated").should("be.visible");
+    cy.get(backlogList).should('be.visible').contains(issueTitle).click();
+    cy.get('[placeholder="Number"]').should('have.value', 10);
+    cy.contains('10h estimated').should('be.visible');
     //Update estimated time
     cy.get('[placeholder="Number"]').clear().type(20);
-    cy.get('[placeholder="Number"]').should("have.value", 20);
-    cy.contains("20h estimated").should("be.visible");
+    cy.get('[placeholder="Number"]').should('have.value', 20);
+    cy.contains('20h estimated').should('be.visible');
     cy.get(closeDetailModalButton).first().click();
     //Assert that updated time is saved
-    cy.get(backlogList).should("be.visible").contains(issueTitle).click();
-    cy.get('[placeholder="Number"]').should("have.value", 20);
-    cy.contains("20h estimated").should("be.visible");
+    cy.get(backlogList).should('be.visible').contains(issueTitle).click();
+    cy.get('[placeholder="Number"]').should('have.value', 20);
+    cy.contains('20h estimated').should('be.visible');
     //Remove estimated time
     cy.get('[placeholder="Number"]').clear();
-    cy.contains("No time logged").should("be.visible");
+    cy.contains('No time logged').should('be.visible');
     cy.get(closeDetailModalButton).first().click();
     //Assert that removed time is saved
-    cy.get(backlogList).should("be.visible").contains(issueTitle).click();
-    cy.contains("No time logged").should("be.visible");
-    cy.get('[placeholder="Number"]').should("have.value", "");
+    cy.get(backlogList).should('be.visible').contains(issueTitle).click();
+    cy.contains('No time logged').should('be.visible');
+    cy.get('[placeholder="Number"]').should('have.value', '');
   });
 
-  it("Should add and remove logged time", () => {
+  it('Should add and remove logged time', () => {
     //Add estimated time
     getIssueDetailsModal().within(() => {
-      cy.contains("No time logged").should("be.visible");
+      cy.contains('No time logged').should('be.visible');
       cy.get('[placeholder="Number"]').type(10);
-      cy.get('[placeholder="Number"]').should("have.value", 10);
-      cy.contains("10h estimated").should("be.visible");
+      cy.get('[placeholder="Number"]').should('have.value', 10);
+      cy.contains('10h estimated').should('be.visible');
       //Add logged time
       cy.get(timeTracking).click();
     });
     getTrackingModal()
-      .should("be.visible")
+      .should('be.visible')
       .within(() => {
         cy.get('[placeholder="Number"]').first().type(2);
         cy.get('[placeholder="Number"]').last().type(5);
-        cy.contains("button", "Done").click();
+        cy.contains('button', 'Done').click();
       });
     //Assert that logged time is saved and estimated time is not visible
-    getTrackingModal().should("not.exist");
-    cy.contains("2h logged").should("be.visible");
-    cy.contains("5h remaining").should("be.visible");
-    cy.contains("No time logged").should("not.exist");
-    cy.contains("10h estimated").should("not.exist");
+    getTrackingModal().should('not.exist');
+    cy.contains('2h logged').should('be.visible');
+    cy.contains('5h remaining').should('be.visible');
+    cy.contains('No time logged').should('not.exist');
+    cy.contains('10h estimated').should('not.exist');
     cy.get(closeDetailModalButton).first().click();
-    cy.get(backlogList).should("be.visible").contains(issueTitle).click();
+    cy.get(backlogList).should('be.visible').contains(issueTitle).click();
     //Delete logged time
     cy.get(timeTracking).click();
     getTrackingModal()
-      .should("be.visible")
+      .should('be.visible')
       .within(() => {
         cy.get('[placeholder="Number"]').first().clear();
         cy.get('[placeholder="Number"]').last().clear();
-        cy.contains("button", "Done").click();
+        cy.contains('button', 'Done').click();
       });
     //Assert that no time is logged
-    cy.contains("No time logged").should("be.visible");
+    cy.contains('No time logged').should('be.visible');
     cy.get(closeDetailModalButton).first().click();
-    cy.get(backlogList).should("be.visible").contains(issueTitle).click();
+    cy.get(backlogList).should('be.visible').contains(issueTitle).click();
     //Assert that estimated time is visible
-    cy.contains("No time logged").should("be.visible");
-    cy.contains("10h estimated").should("be.visible");
+    cy.contains('No time logged').should('be.visible');
+    cy.contains('10h estimated').should('be.visible');
   });
 });
